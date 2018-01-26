52f100ceece7c59bce128e84435b93d358fce83f
hormartin <pigfighter2@gmail.com>
2018-01-26 09:37:21 +0100

    Játék screen (befejezetlen)

---
+++ core/src/com/hakkatoreinbukuma/spaceship/Screens/Game/GameScreen.java
@@ -0,0 +1,28 @@
+package com.hakkatoreinbukuma.spaceship.Screens.Game;
+
+import com.badlogic.gdx.Gdx;
+import com.hakkatoreinbukuma.spaceship.MyBaseClasses.Scene2D.MyScreen;
+import com.hakkatoreinbukuma.spaceship.MyGdxGame;
+
+public class GameScreen extends MyScreen {
+
+    GameStage gameStage;
+
+    public MenuScreen(MyGdxGame game) {
+        super(game);
+        gameStage = new GameStage(spriteBatch, game);
+        Gdx.input.setInputProcessor(gameStage);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        gameStage.act(delta);
+        gameStage.draw();
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
